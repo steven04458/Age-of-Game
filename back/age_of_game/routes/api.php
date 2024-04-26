2b285605f5f9f800e106728c710656d6e32a7ef5
arthurLacombe <arthurlacombe44@gmail.com>
2024-04-26 16:44:10 +0200

    :sparkles: | new route leaderboard

--- back/age_of_game/routes/api.php
+++ back/age_of_game/routes/api.php
@@ -2,13 +2,12 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\controllers\leaderboardController;
 
 Route::post('/login',[UserController::class, 'login']);
 
+Route::get('/leaderboard', [LeaderboardController::class, 'getLeaderboard'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
